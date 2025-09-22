a3057bde50fe6f50a7ecab04a28d726bdcacbdbe
Tejaswini <tejaswinip972002@gmail.com>
2025-09-22 15:06:17 +0530

    Adding Program

---
+++ Console/Program04/Program04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program04                           //Find the largest 3 Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Third Number : ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int largest = a;
+            if (b > largest) largest = b;
+            if (c > largest) largest = c;
+
+            Console.WriteLine("Largest Number is : " + largest);
+        }
+    }
+}
