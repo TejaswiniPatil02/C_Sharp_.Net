dad6bdf2cf46f2d8cb57be97e0a187fcde683f6c
Tejaswini <tejaswinip972002@gmail.com>
2025-09-24 00:43:58 +0530

    Adding program

---
+++ Console/Program05/Program05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Enter Your Name : ");
+            string name = Console.ReadLine();
+
+            Console.Write(" Enter Your Age : " );
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("This isstudent :") ;
+            string isstudent = Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
