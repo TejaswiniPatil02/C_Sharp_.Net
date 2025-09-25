e11b0dc2125041c8052d6d0ae6e0e7cc7751892a
Tejaswini <tejaswinip972002@gmail.com>
2025-09-25 22:38:33 +0530

    Adding Program

---
+++ Console/Program06/Program06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            float price = 99.99f;
+            double largeNumber = 1345.6789;
+            char grade = 'A';
+            string city = "Pune";
+            bool ispassed = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(price);
+            Console.WriteLine(largeNumber);
+            Console.WriteLine(grade);
+            Console.WriteLine(city);
+            Console.WriteLine(ispassed);
+
+            Console.ReadLine();
+
+        }
+    }
+}
