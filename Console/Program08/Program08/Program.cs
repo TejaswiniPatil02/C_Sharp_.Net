2a7f13b85fc60ad2aaeacdd89bbab986e19b6259
Tejaswini <tejaswinip972002@gmail.com>
2025-10-01 13:55:45 +0530

    Adding Program

---
+++ Console/Program08/Program08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assignment Opearator
+
+            int x = 10;
+            x += 5;
+            Console.WriteLine("x += 5: " + x);          // 10+5= 15
+            x *= 15;
+            Console.WriteLine("x *= 5 : " + x);         //15*15= 225
+            x /= 2;
+            Console.WriteLine("x /= 5 : " + x);       // 225/2=112
+
+            Console.ReadLine();
+        }
+    }
+}
