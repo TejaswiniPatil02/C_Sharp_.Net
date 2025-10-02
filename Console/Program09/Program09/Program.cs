088d4b6fbaf8956c7a7f56bd12bfb79b2dd9e347
Tejaswini <tejaswinip972002@gmail.com>
2025-10-02 20:27:14 +0530

    Adding Program

---
+++ Console/Program09/Program09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            if (number < 5)
+            {
+                Console.WriteLine("The Number is grater than 5 ");
+                Console.ReadLine();
+            }
+            else{
+                Console.WriteLine("The Number is less than 5");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
