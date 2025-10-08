bcfb472427225a2437193a8fb449cb75dd96cb4f
Tejaswini <tejaswinip972002@gmail.com>
2025-10-08 22:24:55 +0530

    Adding program

---
+++ Console/Program11/Program11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+        if(number %2 ==0)
+            {
+                Console.WriteLine("Odd Number !!");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Even Number !!!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
