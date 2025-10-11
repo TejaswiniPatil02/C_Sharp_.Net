9194096b59c3b46fcfe8d27ad3dee02321b6d526
Tejaswini <tejaswinip972002@gmail.com>
2025-10-11 14:37:53 +0530

    Adding Progtam

---
+++ Console/Program10/Program10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 90;
+            if (number % 2 == 0)
+                
+            {
+                Console.WriteLine("This is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("This is Odd Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
