7e10bf80c14bd8d7b64e6b2c0b8a640cbefdc383
Tejaswini <tejaswinip972002@gmail.com>
2025-10-12 21:39:38 +0530

    Adding Program

---
+++ Console/program12/program12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Marks : ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+            
+
+            if (marks >= 99)
+            {
+                Console.WriteLine("Grade A !!");
+            }
+            else if (marks >= 80)
+            {
+                Console.WriteLine("Grade B !!");
+            }
+            else if (marks >= 70)
+            {
+                Console.WriteLine("Grade C !!!");
+            }
+            else if (marks >= 60)
+            {
+                Console.WriteLine("Grade D !!!");
+            }
+            else
+            {
+                Console.WriteLine("You Are Not Eligible bcoz Grade D are last You Try Again");
+            }
+            Console.ReadLine();
+        }
+    }
+}
