dfc7648c4dd4cb7b4625ab043521a7c257377c6b
Tejaswini <tejaswinip972002@gmail.com>
2025-10-23 19:49:12 +0530

    Adding program

---
+++ Console/Program15/Program15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Side A : ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Side B : ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is : " +c);
+            Console.ReadKey();
+        }
+    }
+}
