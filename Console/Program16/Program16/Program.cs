74d6586a70bd5192c517721e6b732ba14cb6fa22
Tejaswini <tejaswinip972002@gmail.com>
2025-12-17 12:35:29 +0530

    Adding program

---
+++ Console/Program16/Program16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name : ");
+            string name = Console.ReadLine();
+
+            if(name == "")
+            {
+                Console.WriteLine("You Did Not Enter Your Name !!");
+            }
+            else
+            {
+                Console.WriteLine("Hello" +  name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
