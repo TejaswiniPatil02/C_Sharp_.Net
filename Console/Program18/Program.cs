0437516bec3225df996e0623e8056d62dcc066f5
Tejaswini <tejaswinip972002@gmail.com>
2025-12-26 11:35:48 +0530

    Adding program

---
+++ Console/Program18/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Program18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playagain = true;
+            string player;
+            string computer;
+
+            while(playagain)
+            {
+                player = " ";
+                computer = " ";
+                while( player != "ROCK" && player != "PAPER" && player != " SCISSORS")
+                { 
+                Console.Write("Enter ROCK, PAPER, OR SCISSORS:");
+                player = Console.ReadLine();
+                player = player.ToUpper();
+                }
+
+                switch (random.Next(1,4))
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+
+                }
+
+                Console.WriteLine("player: " + player);
+                Console.WriteLine("computer: " + computer);
+
+                switch (player)
+                {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("Its a draw !!");
+                        }
+                        else if (computer == "PAPER") 
+                        {
+                            Console.WriteLine("You lose !!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Win !!!");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You Win !!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("Its a draw!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose !!!");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You lose !!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You Win !!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Its a draw!!!!");
+                        }
+                        break;
+                }
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
